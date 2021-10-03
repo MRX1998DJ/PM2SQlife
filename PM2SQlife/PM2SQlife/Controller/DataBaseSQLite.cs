4a3a77aa805edac47032fbd9d8a28336f6560eb2
Bayron Muñoz <bayronjosue17@gmail.com>
2021-10-03 17:36:41 -0600

    Agregar archivos de proyecto.

---
+++ PM2SQlife/PM2SQlife/Controller/DataBaseSQLite.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using PM2SQlife.Models;
+using System.Threading.Tasks;
+
+namespace PM2SQlife.Controller
+{
+    public class DataBaseSQLite
+    {
+
+        readonly SQLiteAsyncConnection db;
+
+        //Constructor de la clase DataBaseSQLite
+        public DataBaseSQLite(String pathdb)
+        {
+            //crear una conexion a la base de datos
+            db = new SQLiteAsyncConnection(pathdb);
+
+            //creacion de la tabla personas dentro de SQLite
+            db.CreateTableAsync<Personas>().Wait();
+        }
+
+        //Operaciones CRUD con SQLite
+
+        public Task<List<Personas>> ObtenerListaPersonas()
+        {
+            return db.Table<Personas>().ToListAsync();
+        }
+
+        //READ one by one
+        public Task<Personas> ObtenerPersona(int pcodigo)
+        {
+            return db.Table<Personas>()
+                .Where(i => i.codigo == pcodigo)
+                .FirstOrDefaultAsync();
+        }
+
+        //Create persona
+        public Task<int> GrabarPersona(Personas persona)
+        {
+
+            if (persona.codigo != 0)
+            {
+                return db.UpdateAsync(persona);
+            }
+            else
+            {
+                return db.InsertAsync(persona);
+            }
+        }
+
+        //Delete
+
+        public Task<int> EliminarPersona(Personas persona)
+        {
+            return db.DeleteAsync(persona);
+        }
+
+    }
+}
---
+++ PM2SQlife/PM2SQlife/Models/Personas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace PM2SQlife.Models
+{
+    public class Personas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set;}
+
+        [MaxLength(70)]
+        public String nombre { get; set;}
+
+        [MaxLength(70)]
+        public String apellido { get; set; }
+
+        
+        public int edad { get; set; }
+
+        [MaxLength(100)]
+        public String Direccion { get; set; }
+
+        [MaxLength(100)]
+        public String email { get; set; }
+
+    }
+}
